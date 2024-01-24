3397ed8afa5c0f12a2e84e26fb2a75c8bdfb3d27
Jamesolsson <james.olsson@elev.ga.ntig.se>
2024-01-24 13:44:49 +0100

    start login and reg

---
+++ app.rb
@@ -0,0 +1,52 @@
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+require 'sinatra/reloader'
+
+
+get('/')do
+    slim(:register)
+end
+
+get('/login')do 
+
+end
+
+get('/showlogin')do
+  slim(:login)
+end
+
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/gymi.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+  
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+      redirect('/')
+    else
+      "FEL LÖSENORD"
+    end
+end
+
+post('/users/new')do
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+
+  if (password == password_confirm)
+    password_digest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new('db/gymi.db')
+    db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+    redirect('/login')
+  else
+    "Fel Lösenord"
+  end
+end
+  
